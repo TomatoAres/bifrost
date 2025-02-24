90ee143905c2ca10faa2dd16bc99ca5abf5b531a
Edwin Wang <lark930@gmail.com>
2025-02-24 11:08:21 +0800

    Bump Paseo runtime version

--- runtime/bifrost-paseo/src/lib.rs
+++ runtime/bifrost-paseo/src/lib.rs
@@ -196,7 +196,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bifrost_paseo"),
 	impl_name: create_runtime_str!("bifrost_paseo"),
 	authoring_version: 0,
-	spec_version: 16000,
+	spec_version: 17000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
